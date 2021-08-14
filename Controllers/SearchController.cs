e0bc2dc313b7e63faecbdc88a7e17b8b4d9c5d91
tayyab212 <tthunjra@gmail.com>
2021-08-14 22:42:57 +0500

    initial

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,110 @@
+﻿using CameraSearch.Models;
+using CsvHelper;
+using ExcelDataReader;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TinyCsvParser;
+using TinyCsvParser.Mapping;
+
+namespace CameraSearch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("ConvertCSVtoDataTable111")]
+        public DataTable ConvertCSVtoDataTable()
+        {
+            string strFilePath = "d://cameras-defb.csv";
+            StreamReader sr = new StreamReader(strFilePath);
+            string[] headers = sr.ReadLine().Split(';');
+            DataTable dt = new DataTable();
+            foreach (string header in headers)
+            {
+                dt.Columns.Add(header);
+            }
+            while (!sr.EndOfStream)
+            {
+                //string[] rows = Regex.Split(sr.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                string[] rows = sr.ReadLine().Split(";");
+                DataRow dr = dt.NewRow();
+                try
+                {
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dr[i] = rows[i];
+                    }
+                    dt.Rows.Add(dr);
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            return dt;
+        }
+
+        [HttpGet]
+        [Route("ConvertCSVtoDataTable")]
+        public List<UserDetails> read()
+        {
+            CsvParserOptions csvParserOptions = new CsvParserOptions(true, ',');
+            CsvUserDetailsMapping csvMapper = new CsvUserDetailsMapping();
+            CsvParser<UserDetails> csvParser = new CsvParser<UserDetails>(csvParserOptions, csvMapper);
+            var result = csvParser
+                         .ReadFromFile(@"d://cameras-defb.csv", Encoding.ASCII)
+                         .ToList();
+            Console.WriteLine("Name " + "ID   " + "City  " + "Country");
+            List<UserDetails> cameraDetails = new List<UserDetails>();
+            UserDetails camera = new UserDetails();
+            foreach (var details in result)
+            {
+                try
+                {
+                    string[] Columns = details.Result.ID.Split(";");
+                    //details.Result.Name = Columns[0];
+                    //details.Result.Latitude = Columns[1];
+                    //details.Result.Longitude = Columns[2];
+                    camera.Name = Columns[0];
+                    camera.Latitude = Columns[1];
+                    camera.Longitude = Columns[2];
+                    cameraDetails.Add(camera);
+                }
+                catch (Exception ex)
+                {
+
+
+                }
+
+           
+
+                //Console.WriteLine(details.Result.Name + " " + details.Result.ID + " " + details.Result.City + " " + details.Result.Country);
+            }
+
+            return cameraDetails;
+
+            //         var directory = result
+            //.SelectMany(p => p.Result,
+            //            (parent, child) => new { parent.Result, child.ToString() });
+            //         return result;
+
+        }
+
+
+    }
+
+
+
+}
+
+
+
---
+++ Models/CsvUserDetailsMapping.cs
@@ -0,0 +1,29 @@
+﻿using CameraSearch.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TinyCsvParser.Mapping;
+
+namespace CameraSearch.Models
+{
+    public class CsvUserDetailsMapping : CsvMapping<UserDetails>
+    {
+        public CsvUserDetailsMapping()
+            : base()
+        {
+            MapProperty(0, x => x.ID);
+            //MapProperty(1, x => x.Name);
+            //MapProperty(2, x => x.City);
+            //MapProperty(3, x => x.Country);
+        }
+    }
+
+    public class UserDetails
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
